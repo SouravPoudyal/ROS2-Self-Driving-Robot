6628be450efe265bab343e792c61f53bd4a21066
sourav <souravpoudyal7@gmail.com>
2024-10-04 18:26:05 +0200

    gazebo

--- control/bumperbot_ws/build/bumperbot_description/cmake_install.cmake
+++ control/bumperbot_ws/build/bumperbot_description/cmake_install.cmake
@@ -47,6 +47,7 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
     "/home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/src/bumperbot_description/meshes"
     "/home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/src/bumperbot_description/urdf"
     "/home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/src/bumperbot_description/launch"
+    "/home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/src/bumperbot_description/models"
     "/home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/src/bumperbot_description/rviz"
     )
 endif()
