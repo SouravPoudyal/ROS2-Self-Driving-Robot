dae352572fbf97de3a7b6adc84d7d0f68f42cfee
sourav <souravpoudyal7@gmail.com>
2024-10-04 20:17:38 +0200

    robot_differential_kinematics

--- control/bumperbot_ws/src/bumperbot_controller/launch/controller.launch.py
+++ control/bumperbot_ws/src/bumperbot_controller/launch/controller.launch.py
@@ -1,10 +1,35 @@
 
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.conditions import UnlessCondition, IfCondition
 
 
 def generate_launch_description():
+
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True",
+    )
+    use_python_arg = DeclareLaunchArgument(
+        "use_python",
+        default_value="False",
+    )
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.033",
+    )
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.17",
+    )
+    
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -25,7 +50,29 @@ def generate_launch_description():
         ]
     )
 
+    simple_controller_py = Node(
+                package="bumperbot_controller",
+                executable="simple_controller.py",
+                parameters=[
+                    {"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}],
+                condition=IfCondition(use_python),
+            )
+    simple_controller_cpp = Node(
+                package="bumperbot_controller",
+                executable="simple_controller",
+                parameters=[
+                    {"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}],
+                condition=UnlessCondition(use_python),
+            )
+
     return LaunchDescription([
+            use_python_arg,
+            wheel_radius_arg,
+            wheel_separation_arg,
             joint_state_broadcaster_spawner,
             simple_controller,
+            simple_controller_py,
+            simple_controller_cpp
         ])
