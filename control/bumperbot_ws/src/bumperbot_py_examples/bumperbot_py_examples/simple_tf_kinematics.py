33ddc5379c075d056033e07437235f039b4070fd
sourav <souravpoudyal7@gmail.com>
2024-10-05 14:11:58 +0200

    simple_tf_dynamic

--- control/bumperbot_ws/src/bumperbot_py_examples/bumperbot_py_examples/simple_tf_kinematics.py
+++ control/bumperbot_ws/src/bumperbot_py_examples/bumperbot_py_examples/simple_tf_kinematics.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from geometry_msgs.msg import TransformStamped
+from tf2_ros import TransformBroadcaster
 
 
 class SimpleTfKinematics(Node):
@@ -14,8 +15,12 @@ class SimpleTfKinematics(Node):
 
         # TF Broadcaster
         self.static_tf_broadcaster_ = StaticTransformBroadcaster(self)
-        
+        self.dynamic_tf_broadcaster_ = TransformBroadcaster(self)
+
         self.static_transform_stamped_ = TransformStamped()
+        self.dynamic_transform_stamped_ = TransformStamped()
+
+
         self.static_transform_stamped_.header.stamp = self.get_clock().now().to_msg()
         self.static_transform_stamped_.header.frame_id = "bumperbot_base"
         self.static_transform_stamped_.child_frame_id = "bumperbot_top"
@@ -33,6 +38,28 @@ class SimpleTfKinematics(Node):
                         self.static_transform_stamped_.header.frame_id,
                         self.static_transform_stamped_.child_frame_id
                       ))
+        
+        # Timer
+        self.timer_ = self.create_timer(0.1, self.timerCallback)
+
+    def timerCallback(self):
+
+        self.dynamic_transform_stamped_.header.stamp = self.get_clock().now().to_msg()
+        self.dynamic_transform_stamped_.header.frame_id = "odom"
+        self.dynamic_transform_stamped_.child_frame_id = "bumperbot_base"
+
+
+        self.dynamic_transform_stamped_.transform.translation.x = self.last_x_ + self.x_increment_
+        self.dynamic_transform_stamped_.transform.translation.y = 0.0
+        self.dynamic_transform_stamped_.transform.translation.z = 0.0
+        self.dynamic_transform_stamped_.transform.rotation.x = 0.0
+        self.dynamic_transform_stamped_.transform.rotation.y = 0.0
+        self.dynamic_transform_stamped_.transform.rotation.z = 0.0
+        self.dynamic_transform_stamped_.transform.rotation.w = 1.0
+
+        self.dynamic_tf_broadcaster_.sendTransform(self.dynamic_transform_stamped_)
+        self.last_x_ = self.dynamic_transform_stamped_.transform.translation.x
+
     
 
 def main():
