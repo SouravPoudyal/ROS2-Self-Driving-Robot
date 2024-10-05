358476b65a5eb8b5de3d6e6389b049f879941018
sourav <souravpoudyal7@gmail.com>
2024-10-05 15:42:20 +0200

    simple_service_server

---
+++ control/bumperbot_ws/build/bumperbot_msgs/bumperbot_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/build/bumperbot_msgs/bumperbot_msgs__py
+# Build directory: /home/sourav/ROS2-Self-Driving-Robot/control/bumperbot_ws/build/bumperbot_msgs/bumperbot_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
