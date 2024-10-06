0493f372c480d9fc0fbc33349b953454f460e427
sourav <souravpoudyal7@gmail.com>
2024-10-06 18:54:23 +0200

    wheel_odometry_message

--- control/bumperbot_ws/install/bumperbot_controller/lib/bumperbot_controller/simple_controller.py
+++ control/bumperbot_ws/install/bumperbot_controller/lib/bumperbot_controller/simple_controller.py
@@ -8,6 +8,8 @@ from sensor_msgs.msg import JointState
 from rclpy.time import Time
 from rclpy.constants import S_TO_NS
 import math
+from nav_msgs.msg import Odometry
+from tf_transformations import quaternion_from_euler
 
 
 class SimpleController(Node):
@@ -35,10 +37,20 @@ class SimpleController(Node):
         self.wheel_cmd_pub_ = self.create_publisher(Float64MultiArray, "simple_velocity_controller/commands", 10)
         self.vel_sub_ = self.create_subscription(TwistStamped, "bumperbot_controller/cmd_vel", self.velCallback, 10)
         self.joint_sub_ = self.create_subscription(JointState,"joint_states", self.jointCallback, 10)
+        self.odom_pub_ = self.create_publisher(Odometry, "bumperbot_controller/odom", 10)
 
         self.speed_conversion_ = np.array([[self.wheel_radius_/2, self.wheel_radius_/2],
                                            [self.wheel_radius_/self.wheel_separation_, -self.wheel_radius_/self.wheel_separation_]])
         self.get_logger().info("The conversion matrix is %s" % self.speed_conversion_)
+
+        # Fill the Odometry message with invariant parameters
+        self.odom_msg_ = Odometry()
+        self.odom_msg_.header.frame_id = "odom"
+        self.odom_msg_.child_frame_id = "base_footprint"
+        self.odom_msg_.pose.pose.orientation.x = 0.0
+        self.odom_msg_.pose.pose.orientation.y = 0.0
+        self.odom_msg_.pose.pose.orientation.z = 0.0
+        self.odom_msg_.pose.pose.orientation.w = 1.0
 
 
     def velCallback(self, msg):
@@ -82,6 +94,20 @@ class SimpleController(Node):
         self.theta_ += d_theta
         self.x_ += d_s * math.cos(self.theta_)
         self.y_ += d_s * math.sin(self.theta_)
+
+        # Compose and publish the odom message
+        q = quaternion_from_euler(0, 0, self.theta_)
+        self.odom_msg_.pose.pose.orientation.x = q[0]
+        self.odom_msg_.pose.pose.orientation.y = q[1]
+        self.odom_msg_.pose.pose.orientation.z = q[2]
+        self.odom_msg_.pose.pose.orientation.w = q[3]
+        self.odom_msg_.header.stamp = self.get_clock().now().to_msg()
+        self.odom_msg_.pose.pose.position.x = self.x_
+        self.odom_msg_.pose.pose.position.y = self.y_
+        self.odom_msg_.twist.twist.linear.x = linear
+        self.odom_msg_.twist.twist.angular.z = angular
+        
+        self.odom_pub_.publish(self.odom_msg_)
 
         self.get_logger().info("linear: %f, angular: %f" % (linear, angular))
         self.get_logger().info("x: %f, y: %f, theta: %f" % (self.x_, self.y_, self.theta_))
