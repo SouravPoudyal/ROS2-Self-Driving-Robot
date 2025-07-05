2e018e453e3738a3e913f58e6de38c0598e9fc55
sourav <souravpoudyal7@gmail.com>
2025-07-05 12:06:33 +0200

    lifecycle_manager

--- SLAM/bumperbot_ws/src/bumperbot_localization/launch/global_localization.launch.py
+++ SLAM/bumperbot_ws/src/bumperbot_localization/launch/global_localization.launch.py
@@ -9,6 +9,7 @@ def generate_launch_description():
 
     map_name = LaunchConfiguration("map_name")
     use_sim_time = LaunchConfiguration("use_sim_time")
+    lifecycle_nodes = ["map_server"]
 
     use_sim_time_arg = DeclareLaunchArgument(
         "use_sim_time",
@@ -37,9 +38,22 @@ def generate_launch_description():
             {"use_sim_time": use_sim_time}
         ]
     )
+    
+    nav2_lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        parameters=[
+            {"node_names": lifecycle_nodes},
+            {"use_sim_time": use_sim_time},
+            {"autostart": True}
+        ]
+    )
 
     return LaunchDescription([
         map_name_arg,
         use_sim_time_arg,
-        nav2_map_server
+        nav2_map_server,
+        nav2_lifecycle_manager
     ])
