e3196c5d2cdca9412a052399d1c88396f9d37b60
sourav <souravpoudyal7@gmail.com>
2025-07-09 20:36:23 +0200

    Bresenham_line_algorithm

--- SLAM/bumperbot_ws/src/bumperbot_mapping/bumperbot_mapping/mapping_with_known_poses.py
+++ SLAM/bumperbot_ws/src/bumperbot_mapping/bumperbot_mapping/mapping_with_known_poses.py
@@ -27,7 +27,49 @@ def poseOnMap(pose: Pose, map_info: MapMetaData):
 def poseToCell(pose:Pose, map_info: MapMetaData):
     return map_info.width * pose.y + pose.x
 
+def bresenham(start: Pose, end: Pose):
+    line = []
+    dx = end.x - start.x
+    dy = end.y - start.y
+    xsign = 1 if dx > 0 else -1
+    ysign = 1 if dy > 0 else -1
+    dx = abs(dx)
+    dy = abs(dy)
 
+    if dx > dy:
+        xx = xsign
+        xy = 0
+        yx = 0
+        yy = ysign
+    else:
+        tmp = dx
+        dx = dy
+        dy = tmp
+        xx = 0
+        xy = ysign
+        yx = xsign
+        yy = 0
+
+    D = 2 * dy - dx
+    y = 0
+
+    for i in range(dx + 1):
+        line.append(Pose(start.x + i * xx + y * yx, start.y + i * xy + y * yy))
+        if D >= 0:
+            y += 1
+            D -= 2 * dx
+
+        D += 2 * dy
+
+    return line
+
+def inverseSensorModel(p_robot: Pose, p_beam: Pose):
+    occ_values = []
+    line = bresenham(p_robot, p_beam)
+    for pose in line[:-1]:
+        occ_values.append((pose, 0))
+    occ_values.append((line[-1], 100))
+    return occ_values
 
 class MappingWithKnownPoses(Node):
     def __init__(self, name):
@@ -84,8 +126,10 @@ class MappingWithKnownPoses(Node):
             beam_p = coordinatesToPose(px, py, self.map_.info)
             if not poseOnMap(beam_p, self.map_.info):
                 continue
-            cell = poseToCell(beam_p, self.map_.info)
-            self.map_.data[cell] = 100
+            poses = inverseSensorModel(robot_p, beam_p)
+            for pose, value in poses:
+                cell = poseToCell(pose, self.map_.info)
+                self.map_.data[cell] = value           
         
 
     def timer_callback(self):
